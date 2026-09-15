554773d5c10c1111bdeccb1796709791f9ba7591
Gideon <grinberg1165@gmail.com>
2026-09-15 13:39:29 -0500

    Activate 2027 spring

--- config.js
+++ config.js
@@ -4,8 +4,9 @@ import { fileURLToPath } from "node:url";
 const config = {
     dataDir: path.resolve(path.dirname(fileURLToPath(import.meta.url)), "data"),
     terms: [
+        { name: "2027 Spring", active: true },
         { name: "2026 Fall", active: true },
-        { name: "2026 Summer", active: true },
+        { name: "2026 Summer", active: false },
         { name: "2026 Spring", active: false },
         { name: "2025 Fall", active: false },
         { name: "2025 Spring", active: false },
